f471b15f33c1db2f7335ff9021188f1df0f37b53
Skhushi5 <batabyalsoumi23@gmail.com>
2023-10-12 22:04:52 +0530

    2nd code

---
+++ Missing Number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin>>n;
+    bool arr[n] = {false};
+    for(int i=0;i<n-1;i++){
+        int x; cin>>x;
+        arr[x-1] = true;
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]==false){
+            cout<<i+1;
+            return 0;
+        }
+    }
+    return 0;
+}
