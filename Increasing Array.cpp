382897e051a61f8646a2acae0c2a426a74fbe0ee
Skhushi5 <batabyalsoumi23@gmail.com>
2023-10-13 18:36:26 +0530

    3rd code

---
+++ Increasing Array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#define ll long long
+
+int main() {
+    long long n;
+    cin>> n;
+    long long arr[n];
+    for(ll i =0; i < n ; i ++){
+        cin>> arr[ i];
+
+    }
+    ll ans =0 ;
+    for ( ll i = 1; i <n ; i++ ){
+        if ( arr[i] >= arr[i-1]){
+            continue;
+        }else{
+            ans += arr[i-1] - arr[i]; 
+            arr[i]=arr[i - 1];
+        }
+    } 
+    cout<< ans;
+    return 0;
+}
---
+++ Repititions.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin>> s;
+    int n = s.size();
+    int x=0 ;
+    int max =0;
+    char previous = 'x' ;
+    for(int i =0; i<n ; i ++){
+        if ( s[i] == previous){
+            x++;
+        }else{
+            x=1;
+        }
+        if( x> max){
+            max=x;
+        } 
+        previous =s[i];
+    }
+    cout<< max;
+    return 0;
+}
